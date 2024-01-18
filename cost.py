2a87eea81f8977e82596e4b8db0acae6c32a65a2
Srikanth Chelluri <schellluri.developer@gmail.com>
2024-01-17 21:25:27 -0500

    ensure formatting

--- cost.py
+++ cost.py
@@ -36,15 +36,15 @@ for item in items:
         subtotal_people_to_price[person] += price_per_person
 
 subtotal = sum([item['price'] for item in items])
-print('subtotal', subtotal)
-print('tax', tax)
-print('tip', tip)
+print('subtotal', "%.2f" % subtotal)
+print('tax', "%.2f" % tax)
+print('tip', "%.2f" % tip)
 total = subtotal + tax + tip
-print('total', total, '\n')
+print('total', "%.2f" % total, '\n')
 
 total_people_to_price = defaultdict(float)
 for person, person_subtotal_price in subtotal_people_to_price.items():
     total_people_to_price[person] = (person_subtotal_price / subtotal) * total
 
 for person, person_total_price in total_people_to_price.items():
-    print(person, person_total_price)
+    print(person, "%.2f" % round(person_total_price, 2))
